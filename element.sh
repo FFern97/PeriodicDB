9d0608b7c8a4ce7de25d46e597f4b5ceadbda76e
Facundo Fernández <ffernanndez97@gmail.com>
2024-08-31 01:45:27 -0300

    Update element.sh

--- element.sh
+++ element.sh
@@ -1,59 +1,30 @@
 #!/bin/bash
 
-#if [[ -z $1 ]]; then
-  echo -e "Please provide an element as an argument."
-#fi
-
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
-
-
-SEARCH_ELEMENT() {
-
-SEARCH=$($PSQL "SELECT atomic_number, symbol, name FROM elements WHERE atomic_number = $1 OR symbol = '$1' OR name = '$1'")
-
-if [[ -z $SEARCH ]] 
-  then 
-  echo -e "\n I could not find that element in the database."
-  else
-  echo -e "\nThe element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE_ID, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_PONIT_CELSIUS celsius."
-fi
-
-}
-
-SEARCH_ELEMENT
-
-
-#----------------------------------------------------------------------------------------
-
-#!/bin/bash
-
-if [[ -z $1 ]]; 
-  then
-    echo -e "Please provide an element as an argument."
-    exit 1
-fi
-
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 
+echo -e "Please provide an element as an argument."
+    
+
+
 SEARCH_ELEMENT() {
 
-ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$1")
-
-ELEMENT_SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE symbol= '$1'")
-
-ELEMENT_NAME=$($PSQL "SELECT name FROM elements WHERE name='$1'")
-
-SEARCH=$($PSQL "SELECT atomic_number, symbol, name FROM elements WHERE atomic_number = $1 OR symbol = '$1' OR name = '$1'")
+ 
+SEARCH=$($PSQL "SELECT atomic_number, symbol, name  FROM elements WHERE atomic_number::TEXT = '$1' OR symbol = '$1' OR name = '$1' ")
 
 if [[ -z $SEARCH ]] 
+
   then 
-  echo -e "\n I could not find that element in the database."
+    echo -e "\n I could not find that element in the database."
   else
-  echo -e "\nThe element with atomic number $ATOMIC_NUMBER is '$ELEMENT_NAME' ('$ELEMENT_SYMBOL'). It's a $TYPE_ID, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_PONIT_CELSIUS celsius."
+    echo -e "The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+          
+          #"\nThe element with atomic number $ATOMIC_NUMBER is '$ELEMENT_NAME' ('$ELEMENT_SYMBOL'). It's a $TYPE_ID, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_PONIT_CELSIUS celsius."
 fi
 
 }
 
 SEARCH_ELEMENT $1
+
+
 
