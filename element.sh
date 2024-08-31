5b9e171147f1c6a546039f20dff63fc95c9c343a
Facundo Fernández <ffernanndez97@gmail.com>
2024-08-31 00:27:15 -0300

    Update element.sh

--- element.sh
+++ element.sh
@@ -1,18 +1,21 @@
-echo -e "Please provide an element as an argument."
+#!/bin/bash
+
+#if [[ -z $1 ]]; then
+  echo -e "Please provide an element as an argument."
+#fi
 
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
 
 
-
 SEARCH_ELEMENT() {
 
-read SEARCH
-if
-  SEARCH=($PSQL "SELECT * FROM elements WHERE atomic_number=1 OR symbol='H' OR name=Hydrogen") 
+SEARCH=$($PSQL "SELECT atomic_number, symbol, name FROM elements WHERE atomic_number = $1 OR symbol = '$1' OR name = '$1'")
+
+if [[ -z $SEARCH ]] 
   then 
-  echo -e "\nThe element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
-  else
   echo -e "\n I could not find that element in the database."
+  else
+  echo -e "\nThe element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE_ID, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_PONIT_CELSIUS celsius."
 fi
 
 }
