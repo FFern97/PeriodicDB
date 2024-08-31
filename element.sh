c97f1fbf6bbf3ff21aa9bda3168f882cd20a0710
Facundo Fernández <ffernanndez97@gmail.com>
2024-08-31 18:24:39 -0300

    Update element.sh

--- element.sh
+++ element.sh
@@ -13,14 +13,20 @@ SEARCH_ELEMENT() {
  
 SEARCH=$($PSQL "SELECT atomic_number, symbol, name  FROM elements WHERE atomic_number::TEXT = '$1' OR symbol = '$1' OR name = '$1' ")
 
-if [[ $SEARCH ]] 
+ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number='$1'")
+ELEMENT_NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number='$1'")
+ELEMENT_SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number='$1'")
+MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties WHERE ")
+
+if [[ -z "$SEARCH" ]]
 
   then 
-    echo -e "The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
-  else
     echo -e "\n I could not find that element in the database."
-    exit 1 
-          #"\nThe element with atomic number $ATOMIC_NUMBER is '$ELEMENT_NAME' ('$ELEMENT_SYMBOL'). It's a $TYPE_ID, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOILING_PONIT_CELSIUS celsius."
+    exit 1
+
+  else
+    echo -e "The element with atomic number $ATOMIC_NUMBER is $ELEMENT_NAME ($ELEMENT_SYMBOL). It's a $TYPE_ID, with a mass of $ATOMIC_MASS amu. $ELEMENT_NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius." 
+          
 fi
 }
 
