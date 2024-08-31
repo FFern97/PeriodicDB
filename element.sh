c9942eee6d60fcc3e6cd57496e01c463de2e9294
Facundo Fernández <ffernanndez97@gmail.com>
2024-08-30 22:11:11 -0300

    Update element.sh

--- element.sh
+++ element.sh
@@ -2,10 +2,12 @@ echo -e "Please provide an element as an argument."
 
 SEARCH_ELEMENT() {
 
-read ATOMIC_NUMBER
-read ELEMENT_SYMBOL
-read ELEMENT_NAME
+read SEARCH
 
+
+if
+  SEARCH=($PSQL "SELECT * FROM elements WHERE atomic_number=1 OR symbol='H' OR name=Hydrogen") 
+  echo -e "\nThe element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
 
 
 }
